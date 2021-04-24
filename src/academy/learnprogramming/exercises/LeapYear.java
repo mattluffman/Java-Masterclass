55aedfd3192765494361c800ddbe696f343dbd86
Matt Luffman <matt.luffman@gmail.com>
2021-04-24 16:20:55 -0400

    refactoring if statement

--- src/academy/learnprogramming/exercises/LeapYear.java
+++ src/academy/learnprogramming/exercises/LeapYear.java
@@ -52,10 +52,8 @@ public class LeapYear {
             return false;
         } else if (year % 4 == 0 && year % 100 != 0) {
             return true;
-        } else if (year % 100 == 0 && year % 400 == 0) {
-            return true;
         } else {
-            return false;
+            return year % 100 == 0 && year % 400 == 0;
         }
     }
 }
