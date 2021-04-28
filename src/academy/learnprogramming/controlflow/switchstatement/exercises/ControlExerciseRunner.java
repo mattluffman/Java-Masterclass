9e9e567e1679f9c3539db29b49de340a9259b367
Matt Luffman <matt.luffman@gmail.com>
2021-04-28 18:19:25 -0400

    section 5, exercise 13

--- src/academy/learnprogramming/controlflow/switchstatement/exercises/ControlExerciseRunner.java
+++ src/academy/learnprogramming/controlflow/switchstatement/exercises/ControlExerciseRunner.java
@@ -1,7 +1,8 @@
 package academy.learnprogramming.controlflow.switchstatement.exercises;
 
 import static academy.learnprogramming.controlflow.switchstatement.exercises.NumberInWord.printNumberInWord;
-
+import static academy.learnprogramming.controlflow.switchstatement.exercises.NumberOfDaysInMonth.getDaysInMonth;
+import static academy.learnprogramming.controlflow.switchstatement.exercises.NumberOfDaysInMonth.isLeapYear;
 /**
  * Main method for exercises for switch statements
  *
@@ -12,7 +13,8 @@ import static academy.learnprogramming.controlflow.switchstatement.exercises.Num
 public class ControlExerciseRunner {
 
     public static void main(String[] args) {
-        testNumberInWord();
+        // testNumberInWord();
+        testNumberOfDaysInMonth();
     }
 
     private static void testNumberInWord() {
@@ -28,5 +30,28 @@ public class ControlExerciseRunner {
         printNumberInWord(9);
         printNumberInWord(10);
         printNumberInWord(-23);
+    }
+
+    private static void testNumberOfDaysInMonth() {
+        assert !isLeapYear(1);
+        assert !isLeapYear(-1600);
+        assert isLeapYear(1600);
+        assert !isLeapYear(2017);
+        assert isLeapYear(2000);
+
+        assert getDaysInMonth(1, 1) == -1;
+        assert getDaysInMonth(2, 2020) == 29;
+        assert getDaysInMonth(2, 2021) == 28;
+        assert getDaysInMonth(3, 12) == 31;
+        assert getDaysInMonth(5, 12) == 31;
+        assert getDaysInMonth(7, 12) == 31;
+        assert getDaysInMonth(8, 12) == 31;
+        assert getDaysInMonth(10, 12) == 31;
+        assert getDaysInMonth(12, 12) == 31;
+        assert getDaysInMonth(4, 12) == 30;
+        assert getDaysInMonth(6, 12) == 30;
+        assert getDaysInMonth(9, 12) == 30;
+        assert getDaysInMonth(11, 12) == 30;
+
     }
 }
