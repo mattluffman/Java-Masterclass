9e41537a0bb875ba0af8852e3497bac2b88d2899
Matt Luffman <matt.luffman@gmail.com>
2021-04-28 17:31:50 -0400

    section 5, lecture 64 on switch statements. added the enhanced switch version while he was going over stuff I already knew.

--- src/academy/learnprogramming/controlflow/switchstatement/challenges/DayOfTheWeek.java
+++ src/academy/learnprogramming/controlflow/switchstatement/challenges/DayOfTheWeek.java
@@ -32,6 +32,18 @@ public class DayOfTheWeek {
         printDayOfTheWeek2(6);
         printDayOfTheWeek2(7);
         printDayOfTheWeek2(-27);
+
+        System.out.println(System.lineSeparator());
+
+        printDayOfTheWeek3(0);
+        printDayOfTheWeek3(1);
+        printDayOfTheWeek3(2);
+        printDayOfTheWeek3(3);
+        printDayOfTheWeek3(4);
+        printDayOfTheWeek3(5);
+        printDayOfTheWeek3(6);
+        printDayOfTheWeek3(7);
+        printDayOfTheWeek3(-27);
     }
 
     private static void printDayOfTheWeek(int day) {
@@ -60,6 +72,23 @@ public class DayOfTheWeek {
             default:
                 System.out.println("Invalid day");
                 break;
+        }
+    }
+
+    /**
+     * IDE suggested enhanced switch from java 12/13, not totally clear which
+     * see https://www.vojtechruzicka.com/java-enhanced-switch/
+     */
+    private static void printDayOfTheWeek3(int day) {
+        switch (day) {
+            case 0 -> System.out.println("Sunday");
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            default -> System.out.println("Invalid day");
         }
     }
 
