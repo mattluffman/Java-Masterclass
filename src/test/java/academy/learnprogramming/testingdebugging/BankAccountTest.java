fcbea7f21cdc423c4bd29fb015163c7f636c750e
Matt Luffman <matt.luffman@gmail.com>
2021-05-13 16:58:44 -0400

    section 18, lecture 318 on unit tests. Initial setup with failing tests

---
+++ src/test/java/academy/learnprogramming/testingdebugging/BankAccountTest.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming.testingdebugging;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * @author Matt Luffman
+ * Date: 5/13/2021
+ * Time: 5:02 PM
+ */
+public class BankAccountTest {
+
+    @Test
+    public void getBalance() {
+        fail("This test is yet to be implemented");
+    }
+
+    @Test
+    public void deposit() {
+        fail("This test is yet to be implemented");
+    }
+
+    @Test
+    public void withdraw() {
+        fail("This test is yet to be implemented");
+    }
+}
