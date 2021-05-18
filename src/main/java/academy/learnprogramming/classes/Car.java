384b4d9a3dc1b2d7f57f667ea6c22d21f1ff0ebd
Matt Luffman <matt.luffman@gmail.com>
2021-05-18 00:56:41 -0400

    section 6, lecture 77

---
+++ src/main/java/academy/learnprogramming/classes/Car.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming.classes;
+
+/**
+ * section 6, lecture 77
+ *
+ * @author Matt Luffman
+ * Date: 5/18/2021
+ * Time: 12:42 AM
+ */
+public class Car {
+
+    // state of the car = fields
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+}
---
+++ src/main/java/academy/learnprogramming/classes/Main.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming.classes;
+
+/**
+ * @author Matt Luffman
+ * Date: 5/18/2021
+ * Time: 12:50 AM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+        // porsche.
+    }
+}
