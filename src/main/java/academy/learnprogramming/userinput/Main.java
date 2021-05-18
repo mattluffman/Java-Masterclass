5788f677a0572a05dbb19d29b4ee23f7cd6f44ec
Matt Luffman <matt.luffman@gmail.com>
2021-05-17 21:53:10 -0400

    section 5, lecture 73 - fixing letters input for birth year

--- src/main/java/academy/learnprogramming/userinput/Main.java
+++ src/main/java/academy/learnprogramming/userinput/Main.java
@@ -14,19 +14,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); // handle next line character (enter key)
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
 
-        // calculate age
-        int age = 2021 - yearOfBirth;
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
 
-        if (age >= 0 && age <= 100) {
-            System.out.println("Your name is " + name + ", and you are " + age + " years old");
+            // calculate age
+            int age = 2021 - yearOfBirth;
+
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
         } else {
-            System.out.println("Invalid year of birth");
+            System.out.println("Unable to parse year of birth");
         }
 
         // close for no future input, free up resources
