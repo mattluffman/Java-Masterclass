e2efc9a348364ce8f5c433098f302bc59ccdd3a8
Matt Luffman <matt.luffman@gmail.com>
2022-05-06 16:57:31 -0400

    section 8, exercise 41. Trying this before I get to this section. First 2 methods of exercise are done and tested.

---
+++ src/main/java/academy/learnprogramming/arrays/exercises/SortedArray.java
@@ -0,0 +1,92 @@
+package academy.learnprogramming.arrays.exercises;
+
+import java.util.Scanner;
+
+/**
+ * section 8, exercise 41
+ *
+ * @author Matt Luffman
+ * Date: 5/3/2022
+ * Time: 6:45 PM
+ */
+public class SortedArray {
+
+    /**
+     * Create a program using arrays that sorts a list of integers in descending order.
+     *
+     * Descending order is highest value to lowest.
+     *
+     * In other words if the array had the values in it [106, 26, 81, 5, 15] your program should ultimately have an array with [106, 81, 26, 15, 5] in it.
+     *
+     * Set up the program so that the numbers to sort are read in from the keyboard (Scanner).
+     *
+     * Implement the following methods:
+     *
+     * getIntegers has one parameter of type int which is the size of the array. It returns an array of entered integers from the keyboard.
+     *
+     * printArray accepts an array and prints out the contents of the array. It should be printed in the following format:
+     *
+     * Element 0 contents 106
+     * Element 1 contents 81
+     * Element 2 contents 26
+     * Element 3 contents 15
+     * Element 4 contents 5
+     *
+     * sortIntegers accepts the unsorted array. It should sort the array and return a new array containing the sorted numbers.
+     *
+     * The scenario is:
+     *
+     * 1. getIntegers() is called.
+     *
+     * 2. The returned array from getIntegers() is then used to call sortIntegers().
+     *
+     * 3. The returned array from sortIntegers() is then printed to the console.
+     *
+     * [Do not try and implement this. It is to give you an idea of how the methods will be used]
+     *
+     * TIP: you will have to figure out how to copy the array elements from the passed array into a new array and sort them and return the new sorted array.
+     *
+     * TIP: Instantiate (create) the Scanner object inside the method.
+     *
+     * TIP: Be extremely careful about spaces in the printed message.
+     *
+     * TIP: Make sure the Scanner class is imported.
+     *
+     * NOTE: All methods should be defined as public static NOT public.
+     * NOTE: Do not add a main method to the solution code.
+     */
+
+    public static int[] getIntegers(int arraySize) {
+        int[] numbers = new int[arraySize];
+        Scanner scanner = new Scanner(System.in);
+        int numbersCollected = 0;
+        try {
+            while (true) {
+                if (scanner.hasNextInt()) {
+                    int input = scanner.nextInt();
+                    numbers[numbersCollected] = input;
+                    numbersCollected++;
+
+                    if (numbersCollected == arraySize) {
+                        break;
+                    }
+
+                }
+                scanner.nextLine();
+            }
+        } finally {
+            scanner.close();
+        }
+        return numbers;
+    }
+
+    public static int[] sortIntegers(int[] array) {
+        return null;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("Element %s contents %s%n", i, array[i]);
+        }
+    }
+}
