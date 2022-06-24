3a694ba22144e972d0830fa0b96aebad4576ec17
Matt Luffman <matt.luffman@gmail.com>
2022-06-24 15:58:29 -0400

    section 8, lecture 100 - skeleton with assignment text

---
+++ src/main/java/academy/learnprogramming/arrays/challenges/Main.java
@@ -0,0 +1,30 @@
+package academy.learnprogramming.arrays.challenges;
+
+/**
+ * section 8, lecture 100 - Arrays challenge
+ * Dev note: I started exercise 41 back on 5/6/22. I got everything done except the sorting.
+ * I am not revisiting that example to do this one, I'll do it from scratch again.
+ *
+ * @author Matt Luffman
+ * Date: 6/24/2022
+ * Time: 3:50 PM
+ */
+public class Main {
+
+    /**
+     * Create a program using arrays that sorts a list of integers in descending order.
+     * Descending order is highest value to lowest.
+     * In other words if the array had the values in it 106, 26, 81, 5, 15 your program should
+     * ultimately have an array with 106,81,26, 15, 5 in it.
+     * Set up the program so that the numbers to sort are read in from the keyboard.
+     * Implement the following methods - getIntegers, printArray, and sortIntegers
+     * getIntegers returns an array of entered integers from keyboard
+     * printArray prints out the contents of the array
+     * and sortIntegers should sort the array and return a new array containing the sorted numbers
+     * you will have to figure out how to copy the array elements from the passed array into a new
+     * array and sort them and return the new sorted array.
+     */
+    public static void main(String[] args) {
+
+    }
+}
