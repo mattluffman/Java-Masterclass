2a3aa532b995aa6c3b8da5ab226b1285d6b4d15c
Matt Luffman <matt.luffman@gmail.com>
2022-06-24 15:48:10 -0400

    enhanced for loop suggestion from IDE

--- src/main/java/academy/learnprogramming/arrays/Main.java
+++ src/main/java/academy/learnprogramming/arrays/Main.java
@@ -61,8 +61,10 @@ public class Main {
 
     public static double getAverage(int[] array) {
         int sum = 0;
-        for (int i = 0; i < array.length; ++i) {
-            sum += array[i];
+        // for (int i = 0; i < array.length; ++i) {
+        //     sum += array[i];
+        for (int j : array) {
+            sum += j;
         }
 
         return (double) sum / (double) array.length;
