0de163db0b8736e20d228d36b3f6fd72b5f03f75
Matt Luffman <matt.luffman@gmail.com>
2022-06-24 15:22:09 -0400

    section 8, lecture 99 - 2 ways to initialize arrays

---
+++ src/main/java/academy/learnprogramming/arrays/Main.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming.arrays;
+
+/**
+ * section 8, lecture 99 - Arrays
+ *
+ * @author Matt Luffman
+ * Date: 6/24/2022
+ * Time: 12:11 PM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // int[] myIntArray;// = new int[10];
+        // myIntArray[0] = 45;
+        // myIntArray[1] = 476;
+        // myIntArray[5] = 50; // saves in the 6th spot
+
+        //even better than above
+        int[] myIntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(myIntArray[0]);
+        System.out.println(myIntArray[6]);
+        System.out.println(myIntArray[8]);
+        double[] myDoubleArray = new double[10];
+    }
+}
--- src/main/java/academy/learnprogramming/oop/exercise/DeluxeBurger.java
+++ src/main/java/academy/learnprogramming/oop/exercise/DeluxeBurger.java
@@ -17,21 +17,21 @@ public class DeluxeBurger extends Hamburger {
 
     @Override
     public void addHamburgerAddition1(String name, double price) {
-        System.out.println("Cannot not add additional items to a deluxe burger");
+        System.out.println("Cannot add additional items to a deluxe burger");
     }
 
     @Override
     public void addHamburgerAddition2(String name, double price) {
-        System.out.println("Cannot not add additional items to a deluxe burger");
+        System.out.println("Cannot add additional items to a deluxe burger");
     }
 
     @Override
     public void addHamburgerAddition3(String name, double price) {
-        System.out.println("Cannot not add additional items to a deluxe burger");
+        System.out.println("Cannot add additional items to a deluxe burger");
     }
 
     @Override
     public void addHamburgerAddition4(String name, double price) {
-        System.out.println("Cannot not add additional items to a deluxe burger");
+        System.out.println("Cannot add additional items to a deluxe burger");
     }
 }
