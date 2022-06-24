45922591c25f0637e9f89be65c64e4c04f7b9802
Matt Luffman <matt.luffman@gmail.com>
2022-06-24 15:26:27 -0400

    section 8, lecture 99 - 3rd way to initialize, using a loop

--- src/main/java/academy/learnprogramming/arrays/Main.java
+++ src/main/java/academy/learnprogramming/arrays/Main.java
@@ -10,16 +10,26 @@ package academy.learnprogramming.arrays;
 public class Main {
 
     public static void main(String[] args) {
-        // int[] myIntArray;// = new int[10];
-        // myIntArray[0] = 45;
-        // myIntArray[1] = 476;
-        // myIntArray[5] = 50; // saves in the 6th spot
+        // tedious way to setup
+        int[] myIntArray1 = new int[10];
+        myIntArray1[0] = 45;
+        myIntArray1[1] = 476;
+        myIntArray1[5] = 50; // saves in the 6th spot
 
         //even better than above
-        int[] myIntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(myIntArray[0]);
-        System.out.println(myIntArray[6]);
-        System.out.println(myIntArray[8]);
-        double[] myDoubleArray = new double[10];
+        int[] myIntArray2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(myIntArray2[0]);
+        System.out.println(myIntArray2[6]);
+        System.out.println(myIntArray2[8]);
+
+        // setup using a loop
+        int[] myIntArray3 = new int[10];
+        for (int i = 0; i < 10; ++i) {
+            myIntArray3[i] = i * 10;
+        }
+
+        for (int i = 0; i < 10; ++i) {
+            System.out.println("Element " + i + ", value is " + myIntArray3[i]);
+        }
     }
 }
